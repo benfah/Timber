9a3d736d31bbce801ba65814b151e445fe80a9d1
benfah <vayegame@gmail.com>
2020-02-29 13:55:46 +0100

    Enable slower chopping only for axes

--- src/main/java/me/benfah/timber/mixin/AxeItemMixin.java
+++ src/main/java/me/benfah/timber/mixin/AxeItemMixin.java
@@ -9,6 +9,7 @@ import me.benfah.timber.utils.LogUtils;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.MiningToolItem;
@@ -18,35 +19,32 @@ import net.minecraft.world.World;
 
 @Mixin(AxeItem.class)
 public class AxeItemMixin extends MiningToolItem {
-	
-	
-	protected AxeItemMixin() { super(0, 0, null, null, null); }
 
-	
+	protected AxeItemMixin() {
+		super(0, 0, null, null, null);
+	}
+
 	public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
-		
+
 		if (LogUtils.isLog(state.getBlock()) && !miner.isSneaking()) {
+
 			List<BlockPos> logPositions = new ArrayList<>();
 			boolean hasLeaves = LogUtils.getLogPositions(pos, logPositions, world, state.getBlock(), true);
 			int maxLogs = Math.min(stack.getMaxDamage() - stack.getDamage(), logPositions.size());
-			
-			if(hasLeaves)
-			for (int i = 0; i < maxLogs; i++) {
-				BlockPos logPos = logPositions.get(i);
-				world.breakBlock(logPos, true);
-			}
-			if(!Config.getBoolean("options", "itemDamagePerBlock", true) || !hasLeaves)
-			maxLogs = 0;
-				
+
+			if (hasLeaves)
+				for (int i = 0; i < maxLogs; i++) {
+					BlockPos logPos = logPositions.get(i);
+					world.breakBlock(logPos, true);
+				}
+			if (!Config.getBoolean("options", "itemDamagePerBlock", true) || !hasLeaves)
+				maxLogs = 0;
+
 			stack.damage(maxLogs + 1, miner, (player) -> player.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
 			return true;
 		}
 
 		return super.postMine(stack, world, state, pos, miner);
 	}
-	
-	
-	
-	
-	
+
 }
